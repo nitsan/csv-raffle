ff5c3556765dcae89852f94dd50d468d45e623e2
Nitsan Zohar <nitsanzo@gmail.com>
2020-12-05 13:07:18 +0200

    Update to angular 11

--- src/app/lottery/lottery.component.spec.ts
+++ src/app/lottery/lottery.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LotteryComponent } from './lottery.component';
 
@@ -6,7 +6,7 @@ describe('LotteryComponent', () => {
   let component: LotteryComponent;
   let fixture: ComponentFixture<LotteryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LotteryComponent ]
     })
