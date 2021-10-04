f18b04aa83252a06de6b4070594cbcfa6f8eae75
Nitsan Zohar <nitsanzo@gmail.com>
2021-10-04 23:41:47 +0300

    Add morgan logger and nodemon (#187)

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import path from 'path';
+import morgan from 'morgan';
 
 const app = express();
+app.use(morgan('dev'));
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(
   express.static(path.join(__dirname, '..', 'dist', 'raffle'), {maxAge: 31557600000})
 );
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'raffle', 'index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
