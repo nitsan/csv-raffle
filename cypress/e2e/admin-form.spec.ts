350350699248bb8c43b89c47d4255491cc6ad0b5
nitsan <nitsan@next-insurance.com>
2023-09-07 11:41:51 +0300

    Add percy

--- cypress/e2e/admin-form.spec.ts
+++ cypress/e2e/admin-form.spec.ts
@@ -16,6 +16,7 @@ describe('Admin form', () => {
     AdminFormPage.fillInput(AdminFormSelector.IconUrlInput, logoUrl);
     AdminFormPage.saveAdminForm();
     cy.get(RafflePageSelector.ImageLogo).should('have.attr', 'src', logoUrl);
+    cy.percySnapshot();
   });
 
   it('should change button text', () => {
@@ -23,6 +24,7 @@ describe('Admin form', () => {
     AdminFormPage.fillInput(AdminFormSelector.ButtonText, newText);
     AdminFormPage.saveAdminForm();
     cy.get(RafflePageSelector.StartRaffleBtn).contains(newText);
+    cy.percySnapshot();
   });
 
   it('should change background color', () => {
@@ -33,5 +35,6 @@ describe('Admin form', () => {
       .should('have.value', newColor);
     AdminFormPage.saveAdminForm();
     cy.get(RafflePageSelector.raffleBackground).should('have.css', 'background-color', 'rgb(84, 20, 42)');
+    cy.percySnapshot();
   });
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import '@percy/cypress';
 import './commands';
 
 // Alternatively you can use CommonJS syntax:
